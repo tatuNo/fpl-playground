e278c6502eef5fc1549dea61b751ac8ac2d590fe
tatuNo <Tatu.Nordstrom@metropolia.fi>
2022-05-05 11:12:40 +0300

    base for fetching fpl api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Input, Button } from 'semantic-ui-react';
+import axios from 'axios';
 
-const App = () => (
-  <div>
-    <p>fpl playground</p>
-  </div>
-);
+const App = () => {
+  const [id, setId] = useState(undefined); // my id 3623724
+  const [team, setTeam] = useState([]);
+
+  const handleOnClick = async () => {
+    try {
+      const data = await axios.get(`https://fantasy.premierleague.com/api/entry/${id}/`);
+      setTeam(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(team);
+
+  return (
+    <div>
+      <Input
+        placeholder="Enter team id"
+        onChange={(e) => setId(e.target.value)}
+      />
+      <Button onClick={handleOnClick}>Enter</Button>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    textPrimary: '#24292e',
+    textSecondary: '#586069',
+    textButton: 'white',
+    error: '#d73a4a',
+    primary: '#0366d6',
+    backgroundColorPrimary: '#e1e4e8',
+    backgroundColorSecondary: 'white',
+  },
+};
+
+export default theme;
